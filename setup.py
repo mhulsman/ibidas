3963de02568920566e249a9c4fd1b98c95524f33
Marc Hulsman <m.hulsman@tudelft.nl>
2011-02-16 11:36:51 +0100

    meta info setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ ez_setup.use_setuptools()
 
 from setuptools import setup,find_packages,Extension
 import distutils.sysconfig
+import os
 import os.path
 
 include_dir = distutils.sysconfig.get_python_lib() + "/numpy/core/include/"
@@ -13,6 +14,9 @@ if(not os.path.isfile(os.path.join(include_dir, "numpy/arrayobject.h"))):
     if(not os.path.isfile(os.path.join(include_dir, "numpy/arrayobject.h"))):
         #print os.path.join(include_dir, "numpy/arrayobject.h")
         raise RuntimeError, 'numpy array headers not found'
+
+if not os.path.isdir('docs/_build'):
+    os.mkdir('docs/_build')
 
 setup(
     name="Ibidas",
@@ -39,7 +43,11 @@ setup(
         ),
      ],
      install_requires=['numpy>=1.5.1','sqlalchemy>=0.6.4','ipython>=0.10.1','sphinx>=1.0.5'],
-
+     author = "M. Hulsman & J.J.Bot",
+     author_email = "m.hulsman@tudelft.nl",
+     description = "Ibidas is an environment for data handling and exploration, able to cope with different data structures and sources",
+     url = "https://trac.nbic.nl/ibidas/",
+     license = "LGPLv2.1",
 
 )
 
