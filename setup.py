dc0ff5258d2836633867500dd6022c8a74606ebe
Marc Hulsman <m.hulsman@tudelft.nl>
2012-04-03 00:33:35 +0200

    Update changelog

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ if not os.path.isdir('docs/_build'):
 
 setup(
     name="Ibidas",
-    version="0.1.10",
+    version="0.1.11",
     packages = find_packages(),
     test_suite = "test",
     scripts = ['bin/ibidas'],
