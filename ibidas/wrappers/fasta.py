7012dc01c768daabe93e353970f52c028b54be94
Marc Hulsman <m.hulsman@tudelft.nl>
2012-12-05 12:00:42 +0100

    Prevent reprinting after show, fix bug with csv/tsv storage for single slices, add auto separator discovery for fasta

--- ibidas/wrappers/fasta.py
+++ ibidas/wrappers/fasta.py
@@ -3,7 +3,9 @@ from .. utils import util;
 
 ################################################################################
 
-def read_fasta(fname, sep='|', split=1, fieldnames=()):
+def read_fasta(fname, sep='auto', split=1, fieldnames=()):
+
+    
 
     f = util.open_file(fname,mode='rU');
     fas = [];
@@ -12,10 +14,10 @@ def read_fasta(fname, sep='|', split=1, fieldnames=()):
     seq   = "";
 
     for line in f:
-        line = line.strip();
+        line = line.strip('\n\r');
         if not line or line[0] == ">":
             if seqid:
-                fas = fas + prep_seq_tuple(seqid, seq, sep=sep, split=split);
+                fas.append((seqid, seq))
             seqid = line[1:];
             seq = "";
             continue;
@@ -24,16 +26,25 @@ def read_fasta(fname, sep='|', split=1, fieldnames=()):
     f.close();
 
       # check for one last sequence
-    fas = fas + prep_seq_tuple(seqid, seq, sep=sep, split=split) if seq else fas;
+    if seq:
+        fas.append((seqid,seq))
 
+    if split:
+        if sep == 'auto':
+            sep_search = ['\t','|',',']
+            for sep in sep_search:
+                if all([sep in fas[i][0] for i in xrange(min(len(fas),100))]):
+                    break
+            else:
+                util.warning('Could not determine FASTA separator. Please specify through sep parameter')
+                sep = ''
+
+        if sep:
+            fas = [tuple(x.strip() for x in row[0].split(sep)) + row[1:] for row in fas]
+            
     return Rep(fas)/fieldnames;
 
 
 ################################################################################
 
-def prep_seq_tuple(seqid, seq, sep='|', split=1):
-    seqid = tuple(x.strip() for x in seqid.split(sep)) if split else ( seqid, );
-    return [ seqid + ( seq, ) ];
-
-################################################################################
 
