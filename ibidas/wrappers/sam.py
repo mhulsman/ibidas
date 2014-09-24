c82a6f2172b67fc9eb8ea8480bc1bcbcf881ab47
Thies Gehrmann <thiesgehrmann@gmail.com>
2014-09-24 14:17:31 +0200

    Added SAM file reader and writer. Need to figure out better header storage option.

---
+++ ibidas/wrappers/sam.py
@@ -0,0 +1,90 @@
+from python import Rep;
+from ..utils import util;
+
+##############################################################################
+
+sam_fieldnames = ('qname', 'flag', 'rname', 'pos', 'mapq', 'cigar', 'rnext', 'pnext', 'tlen', 'seq', 'qual', 'optional');
+
+##############################################################################
+
+def read_sam(fname, **kwargs):
+    f   = util.open_file(fname,mode='rU');
+    als = [];
+    hds = "";
+    i   = 0;
+
+    maxfields = len(sam_fieldnames) - 1;
+
+    for line in f:
+      i    = i+1;
+      line = line.strip();
+      if line[0] == '@':
+        hds = hds + line + '\n';
+        continue;
+      #fi
+
+      fields    = line.split('\t', len(sam_fieldnames) - 1);
+      fieldslen  = len(fields);
+      maxfields = maxfields if maxfields > fieldslen else fieldslen;
+
+      print line;
+      print fieldslen, len(sam_fieldnames);
+
+      if fieldslen < len(sam_fieldnames) - 1:
+        util.warning('Line %d does not have all the mandatory fields. Skipping' % i);
+      else:
+        als.append(tuple(fields));
+      #fi
+    #efor
+
+    f.close();
+
+    R = Rep(als) / sam_fieldnames[0:maxfields];
+    R.__dict__['sam_hds'] = hds;
+
+    return R;
+#edef
+
+##############################################################################
+
+def write_sam(data, filename, **kwargs):
+  save_sam_rep(data, open(filename, 'w'));
+#edef
+
+##############################################################################
+
+def save_sam_rep(data, fout):
+
+
+  import md5;
+
+  if len(data.Names) < len(sam_fieldnames) - 1:
+    util.error("This structure doesn't have the correct number of fields. It has %d when it should have at least %d" % ( len(data.Names), len(sam_fieldnames) - 1));
+  #fi
+  if data.Names[0:len(sam_fieldnames)-1] != sam_fieldnames[0:-1]:
+    util.warning("The field names in this structure are not as expected for a SAM structure. Are you sure it is the right format?");
+  #fi
+
+  m = md5.new();
+
+  hds = {};
+  if 'sam_hds' in data.__dict__:
+    hds = data.sam_hds;
+  #fi
+
+  fout.write(hds);
+  m.update(hds);
+
+  lines = zip(*data());
+
+  for line in lines:
+    line = '\t'.join(line) + '\n';
+    fout.write(line);
+    m.update(line);
+  #efor
+
+  return m.hexdigest();
+    
+############################################################################## 
+  
+  
